e0d7a8a7e6ddab6d7787a8b263f85dbe770539e3
Nathaniel Fisher <nathaniel@hivemapper.com>
2023-03-30 14:56:19 -0700

    Me: Can you write the above code to assume there is are two raspberry Pis each with one LoRa Raido?  ChatGPT: Sure, here's an example Python code for a peer-to-peer chat application over LoRaWAN on Raspberry Pi using the sx127x library and a pair of SX127x LoRa radio modules connected to two Raspberry Pis:

--- p2p/chat.py
+++ p2p/chat.py
@@ -10,28 +10,23 @@ coding_rate = CODING_RATE.CR_4_5
 preamble_length = 8
 implicit_header_mode = False
 
-# Initialize LoRa radios
-lora1 = SX127x(frequency=frequency, tx_power_level=tx_power_level, signal_bandwidth=signal_bandwidth,
+# Initialize LoRa radio
+lora = SX127x(frequency=frequency, tx_power_level=tx_power_level, signal_bandwidth=signal_bandwidth,
                spreading_factor=spreading_factor, coding_rate=coding_rate, preamble_length=preamble_length,
                implicit_header_mode=implicit_header_mode, sync_word=None, dio0_pin=25, reset_pin=17, spi_bus=0, spi_device=0)
-lora2 = SX127x(frequency=frequency, tx_power_level=tx_power_level, signal_bandwidth=signal_bandwidth,
-               spreading_factor=spreading_factor, coding_rate=coding_rate, preamble_length=preamble_length,
-               implicit_header_mode=implicit_header_mode, sync_word=None, dio0_pin=25, reset_pin=17, spi_bus=0, spi_device=1)
 
-# Connect to LoRa radios and start receiving messages
-lora1.start()
-lora2.start()
+# Connect to LoRa radio and start receiving messages
+lora.start()
 
 # Callback function to handle incoming messages
 def on_receive(lora, payload):
     print(f"Received message: {payload.decode()}")
 
 # Set callback function for incoming messages
-lora1.set_receive_handler(on_receive)
-lora2.set_receive_handler(on_receive)
+lora.set_receive_handler(on_receive)
 
 # Main loop to send chat messages
 while True:
     message = input("Type a message to send: ")
-    lora1.send(bytes(message, 'utf-8'))
+    lora.send(bytes(message, 'utf-8'))
     print(f"Sent message: {message}")
